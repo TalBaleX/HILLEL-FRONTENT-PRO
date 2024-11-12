eca686bf85455f16a4d8e7fbe9c1988bcb3e9d04
TalBale <pirty43555@gmail.com>
2024-11-12 20:52:51 +0100

    homework 12.11.2024:20:52

---
+++ HW_3/task-2/script.js
@@ -0,0 +1,4 @@
+const prompt1 = prompt("Enter a string");
+const prompt2 = prompt("Enter a string");
+const prompt3 = prompt("Enter a string");
+alert(`${prompt3}\n${prompt2}\n${prompt1}`);
---
+++ HW_3/task-3/script.js
@@ -0,0 +1,4 @@
+const number_5_digits = prompt("Enter a 5-digits number").replace(/\s/g, "");
+alert(
+  `${number_5_digits[0]} ${number_5_digits[1]} ${number_5_digits[2]} ${number_5_digits[3]} ${number_5_digits[4]}`
+);
